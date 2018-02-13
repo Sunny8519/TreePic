2125addd839964c246bd88cf831d69a63e5b17f1
Sunny <niyang56@gmail.com>
2018-02-13 16:14:04 +0800

    new AlertDialogHelper

---
+++ app/src/main/java/com/sunny/learning/treepic/activities/base/SharedMediaActivity.java
@@ -0,0 +1,18 @@
+package com.sunny.learning.treepic.activities.base;
+
+import com.sunny.learning.themeutils.ThemedActivity;
+
+/**
+ * <pre>
+ * author : Sunny
+ * time : 2018/2/13
+ * desc :
+ * </pre>
+ */
+
+public class SharedMediaActivity extends ThemedActivity {
+
+    public void requestSdCardPermissions() {
+
+    }
+}
---
+++ app/src/main/java/com/sunny/learning/treepic/activities/util/AlertDialogHelper.java
@@ -0,0 +1,21 @@
+package com.sunny.learning.treepic.activities.util;
+
+import android.support.annotation.StringRes;
+import android.support.v7.app.AlertDialog;
+
+import com.sunny.learning.themeutils.ThemedActivity;
+
+/**
+ * <pre>
+ * author : Sunny
+ * time : 2018/2/13
+ * desc :
+ * </pre>
+ */
+
+public class AlertDialogHelper {
+
+//    public static AlertDialog getTextDialog(ThemedActivity activity, @StringRes int title, @StringRes int message) {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(activity, activity.getDialogStyle());
+//    }
+}
